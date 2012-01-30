08d5084c485e02e746211686e8e2af54473a4d4f
davetayls <dave@the-taylors.org>
2012-01-30 22:16:23 +0000

    initial files

---
+++ public/js/main.js
@@ -0,0 +1,43 @@
+/**
+* Main
+* ====
+* This is the main js initialiser for the page
+* it is triggered by the data-main attribute on the
+* require script tag.
+* <script data-main="/js/main" src="/lib/require.js"></script>
+* for more information see <http://requirejs.org>
+*/
+/*global define,require,Showdown */
+define.amd.jQuery = true;
+require(
+{ 
+    paths: { 
+        jquery: '../lib/jquery.min',
+        text: '../lib/text',
+        showdown: '../lib/showdown/src/showdown'
+    } 
+}, 
+[ 
+    'jquery',
+    'showdown'
+], 
+function($) {
+    'use strict';
+
+    var $textarea = $('#showdown textarea'),
+        $preview  = $('#preview iframe'),
+        $body     = $preview.contents().find('body'),
+
+        converter = new Showdown.converter()
+    ;
+
+    $textarea.focus(function(){
+        var $this = $(this);
+        $this.keyup(function(){
+            $body.html(
+                converter.makeHtml($this.val())
+            );
+        });
+    });
+
+});
